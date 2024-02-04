59ff90a784a7442c545b1a0a22a5b73f0e2af35e
go-ai-agent <mark.wilsonai@outlook.com>
2024-02-04 06:35:30 -0600

    Updating

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/advanced-go/search
 
 go 1.21.4
 
-require github.com/advanced-go/core v0.0.0-20240203204557-573359f94d04
+require github.com/advanced-go/core v0.0.0-20240204114459-521b4087f86d
 
 require github.com/google/uuid v1.4.0 // indirect
--- provider/startup_test.go
+++ provider/startup_test.go
@@ -3,18 +3,16 @@ package provider
 import (
 	"fmt"
 	"github.com/advanced-go/core/messaging"
-	"github.com/advanced-go/core/uri"
 	"net/http"
 )
 
 func ExampleStartupPing() {
 	r, _ := http.NewRequest("", "github/advanced-go/search/provider:ping", nil)
-	nid, rsc, ok := uri.UprootUrn(r.URL.Path)
-	status := messaging.Ping(nil, nid)
+	status := messaging.Ping(nil, r.URL)
 
-	fmt.Printf("test: Ping() -> [nid:%v] [nss:%v] [ok:%v] [status:%v]\n", nid, rsc, ok, status)
+	fmt.Printf("test: Ping() -> [status:%v]\n", status)
 
 	//Output:
-	//test: Ping() -> [nid:github/advanced-go/search/provider] [nss:ping] [ok:true] [status:200]
+	//test: Ping() -> [status:200]
 
 }
