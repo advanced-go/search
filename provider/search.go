57323720ea5f43ba8e3a8918693fa4d658fad19a
advanced-go <mark.wilson.go@outlook.com>
2024-03-28 07:40:53 -0500

    Updating

--- provider/search.go
+++ provider/search.go
@@ -14,9 +14,6 @@ func search[E runtime.ErrorHandler](ctx context.Context, h http.Header, values u
 		return nil, nil, runtime.NewStatus(http.StatusBadRequest)
 	}
 	var e E
-	//var newCtx context.Context
-	//var status *runtime.Status
-	//var resp *http.Response
 
 	newHeader := make(http.Header)
 	if h != nil {
@@ -26,7 +23,6 @@ func search[E runtime.ErrorHandler](ctx context.Context, h http.Header, values u
 		}
 	}
 	uri := resolver.Build(searchPath, values.Encode())
-	//defer apply(ctx, &newCtx, access.NewRequest(h, http.MethodGet, uri), &resp, googleControllerName, access.StatusCode(&status))()
 	resp, status := exchange.Get(ctx, uri, newHeader)
 	if !status.OK() {
 		return nil, nil, e.Handle(status, runtime.RequestId(h))
@@ -38,17 +34,3 @@ func search[E runtime.ErrorHandler](ctx context.Context, h http.Header, values u
 	resp.ContentLength = int64(len(buf))
 	return buf, resp.Header, status1
 }
-
-/*
-	var buf []byte
-	buf, status = runtime.ReadAll(resp.Body, nil)
-	if !status.OK() {
-		return nil, e.Handle(status, runtime.RequestId(h), searchLocation)
-	}
-	status = runtime.NewStatusOK()
-	status.ContentHeader().Add(http2.ContentType, resp.Header.Get(http2.ContentType))
-	if len(accept) > 0 {
-		status.ContentHeader().Add(runtime.ContentEncoding, resp.Header.Get(runtime.ContentEncoding))
-	}
-
-*/
