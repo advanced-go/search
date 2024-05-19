25f170c3286414bacfb3ebc832a79e64d54186f8
advanced-go <mark.wilson.go@outlook.com>
2024-05-19 12:15:04 -0500

    Update info.go

--- module/info.go
+++ module/info.go
@@ -4,8 +4,10 @@ const (
 	Authority = "github/advanced-go/search"
 	Name      = "search"
 	Version   = "1.1.1"
+)
 
-	// Configuration keys used on startup for map values
+// Configuration keys used on startup for map values
+const (
 	PackageNameUserKey     = "user"    // type:text, empty:false
 	PackageNamePasswordKey = "pswd"    // type:text, empty:false
 	PackageNameRetriesKey  = "retries" // type:int, default:-1, range:0-100
